5c03eeddf0dc9ead550e3b704e740b1174f4c78d
Danil Tolkachev <dandur.oak@gmail.com>
2025-01-16 17:31:36 +0300

    add hpd v2

---
+++ keyboards/ergohaven/hpd/rev1/rules.mk
@@ -0,0 +1,34 @@
+MCU = RP2040
+BOARD = GENERIC_RP_RP2040
+BOOTLOADER = rp2040
+# ALLOW_WARNINGS = yes
+# PICO_INTRINSICS_ENABLED = no
+
+# Build options
+SLEEP_LED_ENABLE = no
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+
+NKRO_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = no
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+NO_USB_STARTUP_CHECK = yes
+RAW_ENABLE = yes
+
+UNICODE_COMMON = yes
+UNICODE_ENABLE = yes
+
+SRC += keyboards/ergohaven/ergohaven_main.c
+SRC += keyboards/ergohaven/ergohaven_ruen.c
+SRC += keyboards/ergohaven/hid.c
+SRC += keyboards/ergohaven/ergohaven_pointing.c
