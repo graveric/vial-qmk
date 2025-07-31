bdf814b37cef069824f73b3691d7dc84b5626d0e
Danil Tolkachev <dandur.oak@gmail.com>
2025-07-31 14:11:50 +0300

    fix mcu

--- keyboards/ergohaven/macropad/rev3/mcuconf.h
+++ keyboards/ergohaven/macropad/rev3/mcuconf.h
@@ -2,8 +2,8 @@
 
 #include_next <mcuconf.h>
 
-#undef RP_SPI_USE_SPI1
-#define RP_SPI_USE_SPI1 TRUE
+#undef RP_SPI_USE_SPI0
+#define RP_SPI_USE_SPI0 TRUE
 
-#undef RP_PWM_USE_PWM0
-#define RP_PWM_USE_PWM0 TRUE
+#undef RP_PWM_USE_PWM3
+#define RP_PWM_USE_PWM3 TRUE
