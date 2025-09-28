eb27db4fc68d638b2ef5dd2c78547943b480e010
Danil Tolkachev <dandur.oak@gmail.com>
2025-09-28 18:08:22 +0300

    fixes with real board

--- keyboards/ergohaven/gamer/keymaps/v1/keymap.c
+++ keyboards/ergohaven/gamer/keymaps/v1/keymap.c
@@ -3,10 +3,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    QK_BOOT,  KC_1,    KC_2,    KC_3,     KC_4,
+    KC_1,     KC_2,    KC_3,    KC_4,     KC_5,
     KC_ESC,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,
     KC_TAB,   KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  KC_LBRC,
-    KC_LGUI, KC_LALT,  KC_LCTL, LOWER,    KC_SPC,   KC_SPC
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  KC_ENTER,
+    KC_LGUI,  KC_NO,   KC_LALT, KC_LCTL,  LOWER,    KC_SPC
   ),
 };
