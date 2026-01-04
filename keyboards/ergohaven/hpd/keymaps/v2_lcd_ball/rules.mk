4e416baf75549c54763467feda44f5a5b79b72fb
Danil Tolkachev <dandur.oak@gmail.com>
2026-01-04 21:05:40 +0300

    move fonts

--- keyboards/ergohaven/hpd/keymaps/v2_lcd_ball/rules.mk
+++ keyboards/ergohaven/hpd/keymaps/v2_lcd_ball/rules.mk
@@ -16,8 +16,8 @@ SRC += keyboards/ergohaven/ergohaven_pointing.c
 
 SRC += keyboards/ergohaven/ergohaven_display.c
 SRC += keyboards/ergohaven/lvgl_helpers.c
-SRC += keyboards/ergohaven/src/fonts/ergohaven_symbols_20.c
-SRC += keyboards/ergohaven/src/fonts/ergohaven_symbols_28.c
+SRC += keyboards/ergohaven/src/display/fonts/ergohaven_symbols_20.c
+SRC += keyboards/ergohaven/src/display/fonts/ergohaven_symbols_28.c
 SRC += keyboards/ergohaven/ergohaven_logo.c
 SRC += keyboards/ergohaven/ergohaven_display_modes.c
 
